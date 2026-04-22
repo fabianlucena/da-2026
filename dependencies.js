49ba7c81bce9e69c24605a559815c7a2ceff01f4
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-04-22 19:49:07 -0300

    Refactor user mockup and update dependencies structure

--- dependencies.js
+++ dependencies.js
@@ -1,6 +1,6 @@
 import { addDependency } from './dependency.js';
 import { UserService } from './services/user_service.js';
-import { UserMockup } from './mockup/user_mockup.js';
+import { UserMockup } from './mockups/user_mockup.js';
 
 addDependency('userService', new UserService());
 addDependency('userRepo', new UserMockup());
