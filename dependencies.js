d3e14d92610c57962678ad9010c3e00c29ccadf0
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-05-20 19:03:45 -0300

    Agregar funcionalidad de inicio de sesión con LoginService y configurar rutas

--- dependencies.js
+++ dependencies.js
@@ -1,6 +1,11 @@
 import { addDependency } from './dependency.js';
 import { UserService } from './services/user_service.js';
+import { LoginService } from './services/login_service.js';
 import userMongo from './mongo-db/users_mongo.js';
+import sessionMongo from './mongo-db/sessions_mongo.js';
 
 addDependency('userRepo', userMongo);
+addDependency('sessionRepo', sessionMongo);
+
 addDependency('userService', new UserService());
+addDependency('loginService', new LoginService());
---
+++ services/login_service.js
@@ -0,0 +1,51 @@
+import { getDependency } from '../dependency.js';
+
+export class LoginService {
+  constructor() {
+    this.userRepo = getDependency('userRepo');
+    this.sessionRepo = getDependency('sessionRepo');
+  }
+
+  createToken() {
+    const array = new Uint8Array(32);
+    crypto.getRandomValues(array);
+    return Array.from(array, byte => byte.toString(16).padStart(2, '0')).join('');
+  }
+
+  async login(data) {
+    if (!data.username)
+      throw new Error('El nombre de usuario es obligatorio');
+
+    if (!data.password)
+      throw new Error('La contraseña es obligatoria');
+
+    const user = await this.userRepo.findOne({
+      username: data.username
+    });
+
+    if (!user)
+      throw new Error('Usuario o contraseña incorrectos');
+
+    if (user.password !== data.password)
+      throw new Error('Usuario o contraseña incorrectos');
+
+    
+    var authorizationToken;
+    do {
+      authorizationToken = this.createToken();
+    } while (await this.sessionRepo.findOne({ authorizationToken }));
+
+    const session = await this.sessionRepo.create({
+      username: user.username,
+      authorizationToken,
+      role: user.role,
+      open: new Date().toISOString(),
+    });
+
+    return {
+      authorizationToken: session.authorizationToken,
+      username: session.username,
+      role: session.role,
+    };
+  }
+}
