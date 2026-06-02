6d49f415a6dc1495e18cfe97f68a179380c8757a
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-06-02 09:55:46 -0300

    Renombrar archivos de modelo de MongoDB y actualizar dependencias en el servidor

--- dependencies.js
+++ dependencies.js
@@ -2,8 +2,8 @@ import { addDependency } from './dependency.js';
 import { UserService } from './services/user_service.js';
 import { LoginService } from './services/login_service.js';
 import { SessionService } from './services/session_service.js';
-import userMongo from './mongo-db/users_mongo.js';
-import sessionMongo from './mongo-db/sessions_mongo.js';
+import userMongo from './mongo-db/user_mongo.js';
+import sessionMongo from './mongo-db/session_mongo.js';
 
 addDependency('userRepo', userMongo);
 addDependency('sessionRepo', sessionMongo);
